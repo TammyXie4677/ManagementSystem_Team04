5158b4a2648b4d1c93a3c5e020e84553e2fa13c4
TammyXie4677 <2474677@johnabbottcollege.net>
2024-11-26 16:07:27 -0500

    Create a CustomerDashBoard

---
+++ CustomerLoyaltyManagementSystem/CustomerLoyaltyManagementSystem/CustomerDashboard.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomerLoyaltyManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for CustomerDashboard.xaml
+    /// </summary>
+    public partial class CustomerDashboard : Window
+    {
+        public CustomerDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Help section is under construction.", "Help", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        // This is the event handler for the button click
+        private void OnViewCustomerLoyaltyButtonClick(object sender, RoutedEventArgs e)
+        {
+            int customerId = 1; // Example: You can pass the actual customer ID here (dynamically assigned in your case)
+            ShowCustomerLoyaltyPage(customerId);
+        }
+
+        // This method will create and show the CustomerLoyaltyPage
+        private void ShowCustomerLoyaltyPage(int customerId)
+        {
+            CustomerLoyaltyPage customerLoyaltyPage = new CustomerLoyaltyPage(customerId);
+            customerLoyaltyPage.Show(); // Show the Customer Loyalty Page
+            this.Hide(); // Optionally hide the main window
+        }
+    }
+}
